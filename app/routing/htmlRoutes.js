cb2e96db97dbc28a54a85b601ed90bb66c24c309
clearplaid <clearplaid@gmail.com>
2019-06-07 10:56:16 -0400

    added routes and linked files together, started build of html on home and survey, just the skeleton, set up server and tested

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,18 @@
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+const path = require("path");
+
+module.exports = function(app) {
 // htmlRoutes.js file should include two routes:
 
 // A GET Route to /survey which should display the survey page.
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
 // A default, catch-all route that leads to home.html which displays the home page.
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+};
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-// server.js file should require the basic npm packages we've used in class: express and path.
-
+// Dependencies
 const express = require('express');
 const path = require('path');
 
+// Sets up the Express App
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Router
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-
-
+// Starts the server to begin listening
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
