4e67885e66a70d666c9b90258ad9c308dbf9dcbe
Scott <clearplaid@gmail.com>
2019-06-10 13:27:27 -0400

    edited a few files to update new plan

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,18 @@
 var friends = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
+        "name": "Aurora",
+        "photo": "",
+        "scores": [
             1,
             2,
+            3,
+            4,
+            5,
             5,
             4,
+            3,
+            2,
             1
-            ]
-          
+        ]
     }
 ]
