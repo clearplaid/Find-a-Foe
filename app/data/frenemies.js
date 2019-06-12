8011a39a9a8677f190994f118f72e4607a37d251
Scott <clearplaid@gmail.com>
2019-06-12 13:50:00 -0400

    added more frenemies to the data

--- app/data/frenemies.js
+++ app/data/frenemies.js
@@ -1,7 +1,71 @@
 var frenemies = [
     {
-        "name":"Badguycat",
-        "photo":"https://i.stack.imgur.com/akcbv.jpg",
+        "name":"Bad Guy Cat",
+        "photo":"http://1.bp.blogspot.com/_U84fPuQrKzI/SGTvXPgavSI/AAAAAAAACl4/08hLMQ6bPjU/s320/_152885.jpg",
+        "scores":[
+            1,
+            2,
+            1,
+            2,
+            1,
+            2,
+            1,
+            2,
+            1,
+            2
+        ]      
+    },
+    {
+        "name": "Good Guy Cat",
+        "photo": "https://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-29-57b30ad229af3__605.jpg",
+        "scores": [
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        "name":"Bad Kitty",
+        "photo":"https://i.pinimg.com/originals/ea/74/68/ea7468e4d12fe5f519f644513736ef57.jpg",
+        "scores":[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+        ]      
+    },
+    {
+        "name": "Good Kitty",
+        "photo": "https://i.pinimg.com/originals/12/7b/6e/127b6ef7b928dce19beb5dcb140fb050.jpg",
+        "scores": [
+            4,
+            4,
+            4,
+            4,
+            4,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name":"Evil Kitty",
+        "photo":"https://media.tenor.com/images/633db42ed616f04635a02979559f7b5f/raw",
         "scores":[
             1,
             1,
@@ -16,19 +80,35 @@ var frenemies = [
         ]      
     },
     {
-        "name": "Goodguycat",
-        "photo": "https://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-29-57b30ad229af3__605.jpg",
+        "name": "Angel Kitty",
+        "photo": "https://static.artfire.com/uploads/mfs/items/c7/a3/large/c7a35efb1efaf217893fb25759f2d791e862b5baa8995924c32f8b5c2f634d8b.jpg",
         "scores": [
-            4,
             5,
-            4,
             5,
-            4,
             5,
-            4,
-            4,
             5,
-            4
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Grumpy Cat",
+        "photo": "https://media.mnn.com/assets/images/2019/01/grumpy_cat.jpg.653x0_q80_crop-smart.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
         ]
     },
 ]
